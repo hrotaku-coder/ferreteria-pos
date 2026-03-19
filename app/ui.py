0d47b9564c0863f408b662fbc9b6a953559c485a
hrotaku-coder <hrotaku@gmail.com>
2026-03-19 13:35:50 -0500

    se agregaron btn NV y BTN Del.

--- app/ui.py
+++ app/ui.py
@@ -18,7 +18,7 @@ frame_top.pack(fill="x")
 
 btn_nueva_venta = tk.Button(
     frame_top,
-    text="Nueva Venta",
+    text="Nueva Venta F1",
     bg="#bfcad3",
     fg="black",
     font=("Arial", 12, "bold"),
@@ -28,6 +28,32 @@ btn_nueva_venta = tk.Button(
 )
 
 btn_nueva_venta.pack(side="left", padx=10, pady=20)
+
+btn_cliente_nuevo = tk.Button(
+    frame_top,
+    text="Cliente Nuevo F2",
+    bg="#bfcad3",
+    fg="black",
+    font=("Arial", 12, "bold"),
+    bd=2,
+    relief="raised",
+    cursor="hand2"    
+)
+
+btn_cliente_nuevo.pack(side="left", padx=10, pady=20)
+
+btn_eliminar_producto = tk.Button(
+    frame_top,
+    text="Elimimar Producto Del.",
+    bg="#bfcad3",
+    fg="black",
+    font=("Arial", 12, "bold"),
+    bd=2,
+    relief="raised",
+    cursor="hand2"    
+)
+
+btn_eliminar_producto.pack(side="left", padx=10, pady=20)
 
 
 # Ejecutar aplicación
