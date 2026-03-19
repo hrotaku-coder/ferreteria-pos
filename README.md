98bd79122ccd0906b39c5d342c96d28e5ad4455a
hrotaku-coder <hrotaku@gmail.com>
2026-03-18 20:48:17 -0500

    avance FLAME Y BOTONES

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # ferreteria-pos
 Sistema POS para ferretería con Python y SQLite
+
+# estilo de bordes
+"flat"     # sin borde (plano)
+"raised"   # elevado (sale hacia afuera)
+"sunken"   # hundido (hacia adentro)
+"ridge"    # borde doble (efecto marco)
+"groove"   # borde tipo ranura
+"solid"    # línea simple
+
+🚀 🔼 SUBIR CAMBIOS (guardar tu trabajo)
+git add .
+git commit -m "mensaje del cambio"
+git push origin main
+
+🔽 ACTUALIZAR PROYECTO (antes de trabajar)
+git pull origin main
+
+🔁 FLUJO COMPLETO (RECOMENDADO)
+
+git pull origin main   # traer cambios
+# haces cambios en tu código
+git add .
+git commit -m "avance"
+git push origin main
+
+🔍 VER ESTADO
+git status
+
+📜 VER HISTORIAL
+git log
+
---
+++ app/ui.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+# Crear ventana principal
+ventana = tk.Tk()
+ventana.title("Ferretería POS")     # Título
+ventana.geometry("1080x800")        # Tamaño
+ventana.configure(bg="#D1D3D5")     #Color ventana
+
+# 🔲 Frame superior
+frame_top = tk.Frame(
+    ventana, 
+    bg="#bfcad3", 
+    height=80,
+    bd=2,           #Grosor borde
+    relief="groove" #Borde Ranura
+)
+frame_top.pack(fill="x")
+
+btn_nueva_venta = tk.Button(
+    frame_top,
+    text="Nueva Venta",
+    bg="#bfcad3",
+    fg="black",
+    font=("Arial", 12, "bold"),
+    bd=2,
+    relief="raised",
+    cursor="hand2"
+)
+
+btn_nueva_venta.pack(side="left", padx=10, pady=20)
+
+
+# Ejecutar aplicación
+ventana.mainloop()
