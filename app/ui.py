a0c653fba13e7b19387762e0669ecc09557d53b7
hrotaku-coder <hrotaku@gmail.com>
2026-03-21 15:46:20 -0500

    avance con los entry

--- app/ui.py
+++ app/ui.py
@@ -15,20 +15,27 @@ frame_top = tk.Frame(
 )
 frame_top.pack(fill="x", padx=10, pady=5)
 
+frame_contenedor = tk.Frame(ventana, bg="#D1D3D5")
+frame_contenedor.pack(fill="x", padx=10, pady=5)
+
 frame_datos = tk.LabelFrame(
     ventana,
     text="Datos Cliente",
     bg="#D1D3D5",
     font=("Arial", 12, "bold"),
     bd=3,
-    relief="groove",
-    height=100
+    relief="groove"
 )
 
-frame_datos.pack(fill="x", padx=10, pady=5)
+frame_datos.pack(anchor="w", padx=10, pady=5)
 
 tk.Label(frame_datos, bg="#D1D3D5", text="NIT/CC:", font=("Arial", 12)).grid(row=0, column=0, padx=5, pady=5)
+entry_documento = tk.Entry(frame_datos, width=25)
+entry_documento.grid(row=0, column=1, padx=5, pady=5, sticky="w")
+
 tk.Label(frame_datos, bg="#D1D3D5", text="Cliente:", font=("Arial", 12)).grid(row=1, column=0, padx=5, pady=5)
+entry_cliente = tk.Entry(frame_datos, width=50)
+entry_cliente.grid(row=1, column=1, padx=5, pady=5, sticky="w")
 
 btn_nueva_venta = tk.Button(
     frame_top,
